5ae55f5624ffd7d30e21a64e2c221e9121c793dd
xdFrenchy <matisduval18magic@gmail.com>
2023-12-08 05:52:18 +0100

    Titi fini

--- script.js
+++ script.js
@@ -31,11 +31,15 @@ function getRandomColor() {
 function applyRandomGradient() {
     const header = document.querySelector('header');
     const footer = document.querySelector('footer');
-    footer.style.background = getRandomGradient();
-    header.style.background = getRandomGradient();
+
+    if(window.location.pathname.includes('cauchemar.html')){
+        footer.style.background = getRandomGradient();
+        header.style.background = getRandomGradient();        
+    }
+
 }
 
-setInterval(applyRandomGradient, 75);
+setInterval(applyRandomGradient, 150);
 // Call the function to apply a random gradient when the page loads
 applyRandomGradient();
 
@@ -60,7 +64,76 @@ function applyRandomFontStyle() {
     const body = document.body;
     body.style.fontFamily = getRandomFontStyle();
 }
+// Function to move the button when the mouse approaches
+function moveButton() {
+    const themeButton = document.getElementById('themebutton');
+    const delay = 2000; // 2 seconds
 
+    // Check if the button is not clickable (during the delay)
+    if (!themeButton.clickable) {
+        // Move the button away
+        themeButton.style.transform = 'translateX(-100%)';
+    }
+}
+
+// Function to make the button clickable
+function makeButtonClickable() {
+    const themeButton = document.getElementById('themebutton');
+
+    // Make the button clickable
+    themeButton.clickable = true;
+
+    // Reset button position
+    themeButton.style.transform = 'translateX(0)';
+}
+
+// Add event listeners to handle mouse approach and button click
+document.getElementById('themebutton').addEventListener('mouseover', moveButton);
+document.getElementById('themebutton').addEventListener('mouseout', makeButtonClickable);
+document.getElementById('themebutton').addEventListener('click', applyRandomGradient);
+
+// Function to show a specific popup image
+function showSpecificPopup(imageName) {
+    const popupOverlay = document.getElementById('popupOverlay');
+    const popupImage = document.getElementById('popupImage');
+
+    // Set the specified image
+    popupImage.src = imageName;
+
+    // Show the popup
+    popupOverlay.style.display = 'flex';
+}
+
+// Set an interval to show a specific popup every 3 seconds
+setInterval(() => {
+    // Choose the specific image you want to show
+    const imageToShow = chooseRandomImageFromSet(); // Implement this function as needed
+    showSpecificPopup(imageToShow);
+}, 3000);
+
+// Function to close the popup
+function closePopup() {
+    const popupOverlay = document.getElementById('popupOverlay');
+    popupOverlay.style.display = 'none';
+}
+
+// Add event listener to close the popup when the close button is clicked
+document.getElementById('popupCloseButton').addEventListener('click', closePopup);
+
+// Function to choose a random image from the specified set
+function chooseRandomImageFromSet() {
+    const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+}
+
+function goToCauchemar() {
+    window.location.href = 'cauchemar.html'
+}
+
+function goToClean(){
+    window.location.href = 'EMY OU CALISTE METTEZ UN TRUC LA'
+}
 // Set an interval to change the font style every second
 setInterval(applyRandomFontStyle, 75);
 
